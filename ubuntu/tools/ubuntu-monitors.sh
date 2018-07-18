014f1553c44e358b9d0a4715ae03e7ccf223a51f
ajshiff <ajshiffler@gmail.com>
2018-07-18 19:35:34 +0000

    Added second copy of updated monitor position to tools folder

---
+++ ubuntu/tools/ubuntu-monitors.sh
@@ -0,0 +1,6 @@
+echo 'Rearranging Monitors...'
+xrandr --output eDP-1 --pos 1280x265
+xrandr --output eDP-1 --pos 1280x265
+xrandr --output eDP-1 --pos 1280x265
+xrandr --output eDP-1 --pos 1280x265
+xrandr --output eDP-1 --pos 1280x265
