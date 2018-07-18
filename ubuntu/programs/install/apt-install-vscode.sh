b202a932a2c180a6c214bb18855c59ed67a42571
ajshiff <ajshiffler@gmail.com>
2018-07-18 13:07:00 +0000

    Changed apt-install-vscode.sh to exclude code-insiders version.

--- ubuntu/programs/install/apt-install-vscode.sh
+++ ubuntu/programs/install/apt-install-vscode.sh
@@ -4,5 +4,5 @@ sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt-get update
 sudo apt-get install -y code
-sudo apt-get install -y code-insiders
+# sudo apt-get install -y code-insiders
 echo '...VSCode Installed.'
